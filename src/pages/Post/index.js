9f9fee47fb16c332193cce570058fea871ced363
vanessa <vanessa.bert311@gmail.com>
2023-10-16 12:06:44 -0300

    Streamlining Post data

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
 import styles from "./Post.module.css";
+import posts from "json/posts.json";
+import PostModel from "components/postModel";
 
 export default function PostPage(){
     const params = useParams();
+    const post = posts.find((post) =>{
+        return post.id === Number(params.id)
+    })
+    console.log(post)
     return(
-        <h1>TESTE {params.id}</h1>
+        <PostModel
+            photo={`/assets/posts/${post.id}/capa2.jpg`}
+            title={post.titulo}
+        >
+            Text
+        </PostModel>
     )
 }
